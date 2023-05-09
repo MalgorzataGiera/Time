b3b4f647639444e77d018382dec7029453907fe0
unknown <gosgie02@wp.pl>
2023-05-09 17:26:18 +0200

    constructors and constructor testing added

--- TimeLib/Time.cs
+++ TimeLib/Time.cs
@@ -7,6 +7,32 @@ namespace TimeLib
         public byte Hours { get; }
         public byte Minutes { get; }
         public byte Seconds { get; }
-        
+
+        public Time(byte? hours = null, byte? minutes = null, byte? seconds = null)
+        {
+            if (hours == null) hours = 0;
+            if (minutes == null) minutes = 0;
+            if (seconds == null) seconds = 0;
+            if (hours > 23 || minutes > 59 || seconds > 59) throw new ArgumentOutOfRangeException();
+
+            Hours = (byte)hours;
+            Minutes = (byte)minutes;
+            Seconds = (byte)seconds;
+        }
+        public Time()
+        {
+            Hours = 0;
+            Minutes = 0;
+            Seconds = 0;
+        }
+        public Time(string s) // for string in format hh:mm:ss or hh.mm.ss
+        {
+            char[] chars = { ':', '.' };
+            string[] data = s.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+            if (byte.Parse(data[0]) > 23 || byte.Parse(data[1]) > 59 || byte.Parse(data[2]) > 59) throw new ArgumentOutOfRangeException();
+            Hours = byte.Parse(data[0]);
+            Minutes = byte.Parse(data[1]);
+            Seconds = byte.Parse(data[2]);
+        }
     }
 }
--- TimeTests1/UnitTest1.cs
+++ TimeTests1/UnitTest1.cs
@@ -4,6 +4,127 @@ namespace TimeTests1
     [TestClass]
     public class UnitTest1
     {
+        #region Constructor tests =======================================================
+        [TestMethod, TestCategory("Constructors")]
+        public void Constructor_Default()
+        {
+            Time t = new Time();
+            Assert.AreEqual((byte)0, t.Hours);
+            Assert.AreEqual((byte)0, t.Minutes);
+            Assert.AreEqual((byte)0, t.Seconds);
+        }
 
+        [TestMethod, TestCategory("Constructors")]
+        [DataRow((byte)1, (byte)25, (byte)13)]
+        [DataRow((byte)01, (byte)25, (byte)13)]
+        public void Constructor_3params(byte h, byte m, byte s)
+        {
+            Time t = new Time(h, m, s);
+            Assert.AreEqual(h, t.Hours);
+            Assert.AreEqual(m, t.Minutes);
+            Assert.AreEqual(s, t.Seconds);
+        }
+
+        [TestMethod, TestCategory("Constructors")]
+        [DataRow((byte)13, (byte)7)]
+        [DataRow((byte)13, (byte)07)]
+        public void Constructor_2params(byte h, byte m)
+        {
+            Time t = new Time(h, m);
+            Assert.AreEqual(h, t.Hours);
+            Assert.AreEqual(m, t.Minutes);
+            Assert.AreEqual((byte)0, t.Seconds);
+        }
+
+        [TestMethod, TestCategory("Constructors")]
+        [DataRow((byte)59)]
+        [DataRow((byte)09)]
+        public void Constructor_1param(byte m)
+        {
+            Time t = new Time(minutes: m);
+            Assert.AreEqual((byte)0, t.Hours);
+            Assert.AreEqual(m, t.Minutes);
+            Assert.AreEqual((byte)0, t.Seconds);
+        }
+
+        [TestMethod, TestCategory("Constructors")]
+        [DataRow("  11:  11 :11    ")]
+        public void Constructor_StringParam_ColonMark(string s)
+        {
+
+            Time t = new Time(s);
+            Assert.AreEqual((byte)11, t.Hours);
+            Assert.AreEqual((byte)11, t.Minutes);
+            Assert.AreEqual((byte)11, t.Seconds);
+        }
+
+        [TestMethod, TestCategory("Constructors")]
+        [DataRow("  11. 11.11    ")]
+        public void Constructor_StringParam_DotMark(string s)
+        {
+
+            Time t = new Time(s);
+            Assert.AreEqual((byte)11, t.Hours);
+            Assert.AreEqual((byte)11, t.Minutes);
+            Assert.AreEqual((byte)11, t.Seconds);
+        }
+
+        // ---------
+
+        [DataTestMethod, TestCategory("Constructors")]
+        [DataRow((byte)24, (byte)0, (byte)0)]
+        [DataRow((byte)0, (byte)60, (byte)0)]
+        [DataRow((byte)0, (byte)0, (byte)60)]
+        [DataRow((byte)24, (byte)60, (byte)0)]
+        [DataRow((byte)24, (byte)0, (byte)60)]
+        [DataRow((byte)0, (byte)60, (byte)60)]
+        [DataRow((byte)24, (byte)60, (byte)60)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_ArgumentOutOfRangeException(byte h, byte m, byte s)
+        {
+            Time t = new Time(h, m, s);
+        }
+
+        [DataTestMethod, TestCategory("Constructors")]
+        [DataRow("24:0:0")]
+        [DataRow("0:60:0")]
+        [DataRow("0:0:60")]
+        [DataRow("24:60:0")]
+        [DataRow("24:0:60")]
+        [DataRow("0:60:60")]
+        [DataRow("24:60:60")]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_StringParam_ColonMark_ArgumentOutOfRangeException(string s)
+        {
+            Time t = new Time(s);
+        }
+
+        [DataTestMethod, TestCategory("Constructors")]
+        [DataRow("24.0.0")]
+        [DataRow("0.60.0")]
+        [DataRow("0.0.60")]
+        [DataRow("24.60.0")]
+        [DataRow("24.0.60")]
+        [DataRow("0.60.60")]
+        [DataRow("24.60.60")]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_StringParam_DotMark_ArgumentOutOfRangeException(string s)
+        {
+            Time t = new Time(s);
+        }
+
+        [DataTestMethod, TestCategory("Constructors")]
+        [DataRow("0,0,0")]
+        [DataRow("0-0-0")]
+        [DataRow("0/0/0")]
+        [DataRow("0;0;0")]
+        [DataRow("0_0_0")]
+        [DataRow("0󪧤")]
+        [ExpectedException(typeof(FormatException))]
+        public void Constructor_StringParam_WrongMark_FormatException(string s)
+        {
+            Time t = new Time(s);
+        }
+        #endregion
     }
 }
